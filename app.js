e6b30f94c4d7f08b1dd9d0e4941eeb4ec8137aac
ponsfrilus <ponsfrilus@gmail.com>
2016-12-01 20:42:28 +0100

    Basic improvements
     * semicolon standardization
     * better HTML source (line break and tabs)
     * submission date in ISO 8601
     * use of `path.sep` for win/linux compatibility
     * check that correct tableHeader as used as dataApp index

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
-var fs = require('fs')
-var path = require('path')
-var glob = require("glob")
-var conf = require('./config.js')
-var dataApp = {}
+var fs = require('fs'),
+  path = require('path'),
+  glob = require("glob"),
+  conf = require('./config.js'),
+  dataApp = {};
 
-fromDir(conf.rootPath, 'informations.json')
+fromDir(conf.rootPath, 'informations.json');
 
 //console.log(dataApp)
-fs.writeFileSync('AllIn.json', JSON.stringify(dataApp), 'UTF-8')
+fs.writeFileSync('AllIn.json', JSON.stringify(dataApp), 'UTF-8');
 
 Object.keys(dataApp).map((formation)=>{
-  var html = '<!DOCTYPE html>\n<html>\n<head>\n\t<meta charset="utf-8" />\n<title>Cadidature ' + formation + '</title>\n</head>\n<body>\n<h2>' + formation + ' (fichier généré le : ' + new Date().toISOString() + ')</h2>\n<table>';
-  html += '\n<tr>' + conf.tableHeaders[formation].map(header => '\n<th>' + header + '</th>').join('') + '\n</tr>';
-  html += dataApp[formation].map(user => '\n<tr>' + conf.tableHeaders[formation].map(header => {
+  var html = '<!DOCTYPE html>\n<html>\n<head>\n\t<meta charset="utf-8" />\n\t<title>Cadidature ' + formation + '</title>\n</head>\n<body>\n\t<h2>' + formation + ' (fichier généré le : ' + new Date().toISOString() + ')</h2>\n\t<table>';
+  html += '\n\t\t\t<tr>' + conf.tableHeaders[formation].map(header => '\n\t\t\t<th>' + header + '</th>').join('') + '\n\t\t\t</tr>';
+  html += dataApp[formation].map(user => '\n\t\t<tr>' + conf.tableHeaders[formation].map(header => {
     switch (header) {
+      case 'datePostulation':
+        var dP = user[header].split('-');
+        return '\n\t\t\t<td>' + dP[2] + '-' + dP[1] + '-' + dP[0] + ' ' + dP[4] + '</td>';
       case 'addresseApprentiComplete':
-        return '\n<td>' + user[header].rue +'<br />'+ user[header].NPA+'</td>';
+        return '\n\t\t\t<td>' + user[header].rue +'<br />'+ user[header].NPA + '</td>';
       case 'connaissancesLinguistiques':
-        return '\n<td>' + user[header].join(', ') +'</td>';
+        return '\n\t\t\t<td>' + user[header].join(', ') + '</td>';
       case 'activitesProfessionnelles':
-        return '\n<td>' + user[header].map(empl => empl.employeur).join('<br />') + '</td>';
+        return '\n\t\t\t<td>' + user[header].map(empl => empl.employeur).join('<br />') + '</td>';
       case 'stages':
-        return '\n<td>' + user[header].map(stage => stage.employeur).join('<br />') + '</td>';
+        return '\n\t\t\t<td>' + user[header].map(stage => stage.employeur).join('<br />') + '</td>';
       default:
-        return '\n<td>' + user[header] + '</td>';
+        return '\n\t\t\t<td>' + user[header] + '</td>';
     }
-    
-  }).join('') + '\n</tr>').join('');
-  
-  html += '</table>\n</body>\n</html>';
+  }).join('') + '\n\t\t</tr>').join('');
+
+  html += '\n\t</table>\n</body>\n</html>';
   console.log(html);
   fs.writeFileSync('./results/'+formation+'.html', html, 'UTF-8');
-
-})
+});
 
 function fromDir(startPath, filter) {
   if (!fs.existsSync(startPath)) {
@@ -48,27 +49,17 @@ function fromDir(startPath, filter) {
     if (stat.isDirectory()) {
       fromDir(filename, filter); //recurse
     } else if (filename.indexOf(filter) >= 0) {
-      // console.log('— found: ',filename);
       // Read the JSON file synchronously
       var obj = JSON.parse(fs.readFileSync(filename, 'utf8'));
-      var form = filename.split('\\')[2]
-      // console.log(obj);
-      if(!dataApp[form]){
-        dataApp[form] = []
+      var form = filename.split(path.sep)[1];
+      if (form in conf.tableHeaders === false) {
+        console.log("The lookup index is not in table header, please check how the filename is splitted in fromDir function in app.js", form);
+        return;
       }
-      dataApp[form].push(obj)
+      if (!dataApp[form]){
+        dataApp[form] = [];
+      }
+      dataApp[form].push(obj);
     }
   }
 }
-
-/*
-formations = {
-  Informaticien: [
-    { sciper: 'g13123'},
-    { sciper: 'g13123'},
-    { sciper: 'g13123'}
-  ],
-
-}
-
-*/
